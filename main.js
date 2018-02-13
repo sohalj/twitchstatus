c72ff6e85d4a7223a474b733ed42ddadf3fc42f5
sohalj <sohalj@students.wwu.edu>
2018-02-13 10:53:24 -0800

    javascript logic comepleted for all display

--- main.js
+++ main.js
@@ -1,54 +1,4 @@
 
- /* console.log("in display")
- var l = document.createElement("li");
- var p = document.createElement("p");
- p.setAttribute("style", "float:left");
- var i = document.createElement("img");
- setAttribute(i, { 'class': 'logo img-responsive', src: r.logo});
- p.appendChild(i);
- var p2 = document.createElement("p");
- p2.setAttribute("style", "float:right");
- var i2 = document.createElement("i");
- //setAttribute(i2, { 'class': 'fa fa-exclamation'});
- setAttribute(i2, { 'class': 'fa fa-check'});
- console.log(i2)
- p2.appendChild(i2);
- l.appendChild(p);
- l.appendChild(p2);
- var h = document.createElement("a");
- h.setAttribute("href", r.url);
- h.setAttribute("target", "_blank");
- var t = document.createTextNode(r.display_name);
- h.appendChild(t);
- l.appendChild(h);
- console.log("fasdf")
- console.log(h)
- var s = document.createElement("p");
- var status = document.createTextNode(r.status);
- s.appendChild(status)
- l.appendChild(s);
-
- console.log(l)
-
- var a = document.getElementById("list");
- a.appendChild(l);
-
-*/
-
-
-///display used with channel calls
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -62,35 +12,43 @@ function setAttribute(elem, attr){
 
 
 
-function display(r){
+function display(logo, stream, url, name, status){
+  var symbol = null;
+  
+  if (stream == 0){
+    symbol = 'fa fa-exclamation';
+  }
+  else{
+    symbol = 'fa fa-check';
+  }
 
 
-  console.log("in display")
+
+ console.log("in display")
  var l = document.createElement("li");
  var p = document.createElement("p");
  p.setAttribute("style", "float:left");
  var i = document.createElement("img");
- setAttribute(i, { 'class': 'logo img-responsive', src: r.stream.channel.logo});
+ setAttribute(i, { 'class': 'logo img-responsive', src: logo});
  p.appendChild(i);
  var p2 = document.createElement("p");
  p2.setAttribute("style", "float:right");
  var i2 = document.createElement("i");
- //setAttribute(i2, { 'class': 'fa fa-exclamation'});
- setAttribute(i2, { 'class': 'fa fa-check'});
+ setAttribute(i2, { 'class': symbol});
  console.log(i2)
  p2.appendChild(i2);
  l.appendChild(p);
  l.appendChild(p2);
  var h = document.createElement("a");
- h.setAttribute("href", r.stream.channel.url);
+ h.setAttribute("href", url);
  h.setAttribute("target", "_blank");
- var t = document.createTextNode(r.stream.channel.display_name);
+ var t = document.createTextNode(name);
  h.appendChild(t);
  l.appendChild(h);
  console.log("fasdf")
  console.log(h)
  var s = document.createElement("p");
- var status = document.createTextNode(r.stream.channel.status);
+ var status = document.createTextNode(status);
  s.appendChild(status)
  l.appendChild(s);
 
@@ -109,24 +67,22 @@ function display(r){
 
 
 
-function search(){
-    //var a = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-
-  //for(i=0; i<a.length; i++){
+function search(type, name){
+    var r = null;
+    console.log(type + "value of type");
+    console.log(name + " value of name")
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function (){
       if(this.readyState == 4 && this.status == 200){
-        var r = JSON.parse(this.responseText);
+        r = JSON.parse(this.responseText);
         console.log(r);
         
-        //console.log(r.stream.channel.display_name);
-        //console.log(r.stream.channel.url);
-        display(r);
       }
     };  
-    xhr.open("GET", "https://wind-bow.glitch.me/twitch-api/streams/" + "MedryBW" , true);
+    xhr.open("GET", "https://wind-bow.glitch.me/twitch-api/" + type + "/" + name, false);
+    //xhr.open("GET", "https://wind-bow.glitch.me/twitch-api/streams/" + "MedryBW" , true);
     xhr.send();
-  //}  
+    return r;
 }
 
 
@@ -135,6 +91,25 @@ function search(){
 
 
 $(document).ready(function(){
-  search();
-
+  var a = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  console.log("before search");
+  for(i=0; i<a.length;i++){
+    var s = 'streams';
+    var c = 'channels';
+    var name = 'MedryBW';
+    var v = search(s, a[i]);
+    console.log("after search");
+    console.log(v);
+    if(v.stream != null){
+      display(v.stream.channel.logo, 1, v.stream.channel.url, v.stream.channel.display_name, v.stream.channel.status,);
+      console.log(v);
+      console.log("cannel not stream call search again with channels")
+    }
+    else{
+      v = search(c, a[i]);
+      display(v.logo, 0, v.url, v.display_name, "");
+      console.log(v);
+      console.log("channel worked")
+    }
+  }
 });
