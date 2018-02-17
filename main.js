5bf7c86465f2d299e5da1e9a6c035fcd6517b8d0
sohalj <sohalj@students.wwu.edu>
2018-02-16 19:16:03 -0800

    javascript working look for potential ways to increase performance

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 
-
-
-
 function setAttribute(elem, attr){
   Object.keys(attr).forEach(key => elem.setAttribute(key, attr[key]));
 }
 
 
-
-
-
-
-
 function display(logo, stream, url, name, status){
   var symbol = null;
-  
   if (stream == 0){
     symbol = 'fa fa-exclamation';
   }
@@ -22,68 +13,103 @@ function display(logo, stream, url, name, status){
     symbol = 'fa fa-check';
   }
 
+   console.log("in display")
+   var l = document.createElement("li");
+   var p = document.createElement("p");
+   p.setAttribute("style", "float:left");
+   var i = document.createElement("img");
+   setAttribute(i, { 'class': 'logo img-responsive', src: logo});
+   p.appendChild(i);
+   var p2 = document.createElement("p");
+   p2.setAttribute("style", "float:right");
+   var i2 = document.createElement("i");
+   setAttribute(i2, { 'class': symbol});
+   //console.log(i2)
+   p2.appendChild(i2);
+   l.appendChild(p);
+   l.appendChild(p2);
+   var h = document.createElement("a");
+   setAttribute(h, {href: url, target: '_blank'});
+   var t = document.createTextNode(name);
+   h.appendChild(t);
+   l.appendChild(h);
+   //console.log("fasdf")
+   //console.log(h)
+   var s = document.createElement("p");
+   var status = document.createTextNode(status);
+   s.appendChild(status)
+   l.appendChild(s);
 
+   //console.log("value of l");
+   //console.log(l)
 
- console.log("in display")
- var l = document.createElement("li");
- var p = document.createElement("p");
- p.setAttribute("style", "float:left");
- var i = document.createElement("img");
- setAttribute(i, { 'class': 'logo img-responsive', src: logo});
- p.appendChild(i);
- var p2 = document.createElement("p");
- p2.setAttribute("style", "float:right");
- var i2 = document.createElement("i");
- setAttribute(i2, { 'class': symbol});
- console.log(i2)
- p2.appendChild(i2);
- l.appendChild(p);
- l.appendChild(p2);
- var h = document.createElement("a");
- h.setAttribute("href", url);
- h.setAttribute("target", "_blank");
- var t = document.createTextNode(name);
- h.appendChild(t);
- l.appendChild(h);
- console.log("fasdf")
- console.log(h)
- var s = document.createElement("p");
- var status = document.createTextNode(status);
- s.appendChild(status)
- l.appendChild(s);
-
- console.log(l)
-
- var a = document.getElementById("list");
- a.appendChild(l);
+   var a = document.getElementById("list");
+   //console.log(a);
+   a.appendChild(l);
 }
 
 
 
 
-
-
-
-
-
-
-function search(type, name){
-    var r = null;
-    console.log(type + "value of type");
-    console.log(name + " value of name")
+function search(a, all, on, off){
+  $('#list').empty();
+  var size = a.length
+  var type = 'streams';
+  var stream = true;
+  
+  for(i=0; i<size; i++){
+    var name = a[i];
+    console.log("value of name   " + a[i]);
     var xhr = new XMLHttpRequest();
+    console.log("value of i     " + i);
     xhr.onreadystatechange = function (){
       if(this.readyState == 4 && this.status == 200){
-        r = JSON.parse(this.responseText);
-        console.log(r);
-        
+        var r = JSON.parse(this.responseText);               
+        if(r.stream != null && (on || all)){  
+          console.log("WE HAVE A STREAMER");
+          console.log(r);
+          console.log("stream display being called");
+          display(r.stream.channel.logo, 1, r.stream.channel.url, r.stream.channel.display_name, r.stream.channel.status);
+        }
+        else if (r.stream == null && (off || all)){
+          var link = r._links.channel.slice(38, r._links.channel.length); ///have to use slice since non-streaming channels only return links that would work with a API key
+          var xhr = new XMLHttpRequest();
+          xhr.open("GET", "https://wind-bow.glitch.me/twitch-api/channels/" + link, true);
+          console.log(" BEFORE sending API  inside ELSE call");
+          xhr.send();
+          xhr.onreadystatechange = function (){
+            if(this.readyState == 4 && this.status == 200){
+              var r = JSON.parse(this.responseText);
+              display(r.logo, 0, r.url, r.display_name, "");
+            }
+            else{
+              //exception handler
+            }
+          }   
+
+        }
+      else{
+                  //exceptionhandler
       }
+
+    }
+    else{
+          ////exception handler  
+    }
     };  
-    xhr.open("GET", "https://wind-bow.glitch.me/twitch-api/" + type + "/" + name, false);
-    //xhr.open("GET", "https://wind-bow.glitch.me/twitch-api/streams/" + "MedryBW" , true);
-    xhr.send();
-    return r;
+      xhr.open("GET", "https://wind-bow.glitch.me/twitch-api/" + type + "/" + name, true);
+      xhr.send();
+  }
+
 }
+
+
+
+
+
+
+
+
 
 
 
@@ -91,25 +117,24 @@ function search(type, name){
 
 
 $(document).ready(function(){
-  var a = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-  console.log("before search");
-  for(i=0; i<a.length;i++){
-    var s = 'streams';
-    var c = 'channels';
-    var name = 'MedryBW';
-    var v = search(s, a[i]);
-    console.log("after search");
-    console.log(v);
-    if(v.stream != null){
-      display(v.stream.channel.logo, 1, v.stream.channel.url, v.stream.channel.display_name, v.stream.channel.status,);
-      console.log(v);
-      console.log("cannel not stream call search again with channels")
-    }
-    else{
-      v = search(c, a[i]);
-      display(v.logo, 0, v.url, v.display_name, "");
-      console.log(v);
-      console.log("channel worked")
-    }
-  }
+  var a = ["ESL_SC2", "noobs2ninjas", "freecodecamp", "cretetion", "storbeck", "habathcx", "OgamingSC2", "RobotCaleb"];
+  //var a = [ "noobs2ninjas", "freecodecamp", "cretetion", "storbeck", "habathcx", "RobotCaleb"];
+  //var a = ["noobs2ninjas", "MedryBW", "ESL_SC2"];
+  //var a = ["noobs2ninjas"];
+
+  search(a, true, false, false);
+
+
+  $('#online').click(function(){
+    search(a, false, true, false)
+  });
+
+  $('#offline').click(function(){
+    search(a, false, false, true);
+  });
+
+  $('#all').click(function(){
+    search(a, true, false, false)
+  });
+
 });
