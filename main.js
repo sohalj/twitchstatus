45bce31b8d4c5f1d781799908bb3e62a30d44c83
sohalj <sohalj@students.wwu.edu>
2018-02-26 20:37:21 -0800

    fixed performance issues need to complete css

--- main.js
+++ main.js
@@ -13,8 +13,26 @@ function display(logo, stream, url, name, status){
     symbol = 'fa fa-check';
   }
 
-   console.log("in display")
+  /* console.log("in display")
    var l = document.createElement("li");
+   l.innerHTML += "list itme";
+
+    var a = document.getElementById("list");
+   console.log(a);
+   a.appendChild(l);*/
+
+
+  var l = document.createElement("li");
+  if(stream == 0){
+  	l.setAttribute("class", "off")
+  }
+  else{
+  	l.setAttribute("class", "on")
+  }
+
+
+
+
    var p = document.createElement("p");
    p.setAttribute("style", "float:left");
    var i = document.createElement("img");
@@ -36,6 +54,7 @@ function display(logo, stream, url, name, status){
    //console.log("fasdf")
    //console.log(h)
    var s = document.createElement("p");
+   s.setAttribute('class', 'status');
    var status = document.createTextNode(status);
    s.appendChild(status)
    l.appendChild(s);
@@ -44,42 +63,44 @@ function display(logo, stream, url, name, status){
    //console.log(l)
 
    var a = document.getElementById("list");
-   //console.log(a);
+   console.log(a);
    a.appendChild(l);
 }
 
 
 
 
-function search(a, all, on, off){
+function search(a){
   $('#list').empty();
   var size = a.length
   var type = 'streams';
   var stream = true;
+  var name = null;
   
   for(i=0; i<size; i++){
-    var name = a[i];
-    console.log("value of name   " + a[i]);
+    name = a[i];
+    //console.log("value of name   " + a[i]);
     var xhr = new XMLHttpRequest();
     console.log("value of i     " + i);
     xhr.onreadystatechange = function (){
       if(this.readyState == 4 && this.status == 200){
         var r = JSON.parse(this.responseText);               
-        if(r.stream != null && (on || all)){  
-          console.log("WE HAVE A STREAMER");
-          console.log(r);
-          console.log("stream display being called");
+        if(r.stream != null){  
+      //    console.log("WE HAVE A STREAMER");
+        //  console.log(r);
+          //console.log("stream display being called");
           display(r.stream.channel.logo, 1, r.stream.channel.url, r.stream.channel.display_name, r.stream.channel.status);
         }
-        else if (r.stream == null && (off || all)){
+        else if (r.stream == null){
           var link = r._links.channel.slice(38, r._links.channel.length); ///have to use slice since non-streaming channels only return links that would work with a API key
           var xhr = new XMLHttpRequest();
-          xhr.open("GET", "https://wind-bow.glitch.me/twitch-api/channels/" + link, true);
-          console.log(" BEFORE sending API  inside ELSE call");
+          xhr.open("GET", "https://wind-bow.glitch.me/twitch-api/channels/" + link, true); /// all these calls occur after all orginal calls have taken place thats why have to use link
+        //  console.log(" BEFORE sending API  inside ELSE call");
           xhr.send();
           xhr.onreadystatechange = function (){
             if(this.readyState == 4 && this.status == 200){
               var r = JSON.parse(this.responseText);
+              console.log(r);
               display(r.logo, 0, r.url, r.display_name, "");
             }
             else{
@@ -119,22 +140,25 @@ function search(a, all, on, off){
 $(document).ready(function(){
   var a = ["ESL_SC2", "noobs2ninjas", "freecodecamp", "cretetion", "storbeck", "habathcx", "OgamingSC2", "RobotCaleb"];
   //var a = [ "noobs2ninjas", "freecodecamp", "cretetion", "storbeck", "habathcx", "RobotCaleb"];
-  //var a = ["noobs2ninjas", "MedryBW", "ESL_SC2"];
+ // var a = ["noobs2ninjas", "MedryBW", "ESL_SC2"];
   //var a = ["noobs2ninjas"];
 
-  search(a, true, false, false);
+  search(a);
 
 
   $('#online').click(function(){
-    search(a, false, true, false)
+  	$('.off').hide();
+  	$('.on').show();
   });
 
   $('#offline').click(function(){
-    search(a, false, false, true);
+  	$('.on').hide();
+  	$('.off').show();
   });
 
   $('#all').click(function(){
-    search(a, true, false, false)
+  	$('.on').show();
+  	$('.off').show();
   });
 
 });
